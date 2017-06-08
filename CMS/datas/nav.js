d9abb2f17832768e99be85051c50e2d55885c8e0
chuwu <1178078758@qq.com>
2017-06-08 18:16:50 +0800

    no message

---
+++ CMS/datas/nav.js
@@ -0,0 +1,167 @@
+var navs = [
+	{
+		"title": "菜单列表",
+		"icon": "",
+		"spread": false
+	},{
+		"title": "系统管理",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "系统用户列表",
+			"icon": "&#xe641;",
+			"href": "system/sysUserList.html"
+		},{
+			"title": "系统角色列表",
+			"icon": "&#xe63c;",
+			"href": "system/systemRole.html"
+		}, {
+			"title": "创建角色",
+			"icon": "&#xe63c;",
+			"href": "system/createRole.html"
+		}, {
+			"title": "创建用户",
+			"icon": "&#xe609;",
+			"href": "system/createUser.html"
+		}, {
+			"title": "客服管理",
+			"icon": "&#xe60c;",
+			"href": "system/customerService.html"
+		}]
+	},{
+		"title": "用户管理",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "用户列表",
+			"icon": "&#xe641;",
+			"href": "userAdmin/userList.html"
+		},{
+			"title": "用户充值记录",
+			"icon": "&#xe60c;",
+			"href": "userAdmin/userRechaargeRecord.html"
+		}]
+	},{
+		"title": "宝贝管理",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "宝贝列表",
+			"icon": "&#xe641;",
+			"href": "babyAdmin/babyList.html"
+		},{
+			"title": "添加宝贝",
+			"icon": "&#xe60c;",
+			"href": "babyAdmin/createBaby.html"
+		},{
+			"title": "宝贝审核",
+			"icon": "&#xe60c;",
+			"href": "babyAdmin/babyExa.html"
+		},{
+			"title": "服务日志",
+			"icon": "&#xe60c;",
+			"href": "babyAdmin/serverJour.html"
+		},{
+			"title": "宝贝状态管理",
+			"icon": "&#xe60c;",
+			"href": "babyAdmin/babyStatusAdimin.html"
+		}]
+	},{
+		"title": "订单管理",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "订单列表",
+			"icon": "&#xe641;",
+			"href": "orderList.html"
+		},{
+			"title": "进行中订单",
+			"icon": "&#xe60c;",
+			"href": "conductOrder.html"
+		},{
+			"title": "紧急订单",
+			"icon": "&#xe60c;",
+			"href": "urgentOrder.html"
+		},{
+			"title": "预定包厢",
+			"icon": "&#xe60c;",
+			"href": "reserveBox.html"
+		},{
+			"title": "退款申请",
+			"icon": "&#xe60c;",
+			"href": "refundRequest.html"
+		}]
+	},{
+		"title": "其他管理",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "等级管理",
+			"icon": "&#xe641;",
+			"href": "gradeAdmin.html"
+		},{
+			"title": "服务范围管理",
+			"icon": "&#xe63c;",
+			"href": "serverRangeAdmin.html"
+		}, {
+			"title": "Banner管理",
+			"icon": "&#xe63c;",
+			"href": "bannerAdmin.html"
+		}, {
+			"title": "服务场所管理",
+			"icon": "&#xe609;",
+			"href": "severPlaceAdmin.html"
+		}, {
+			"title": "约会地址管理",
+			"icon": "&#xe60c;",
+			"href": "appAddressAmin.html"
+		}]
+	},{
+		"title": "提现管理",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "提现申请",
+			"icon": "&#xe641;",
+			"href": "wtfApply.html"
+		},{
+			"title": "提现记录",
+			"icon": "&#xe63c;",
+			"href": "wtfRecord.html"
+		}]
+	},{
+		"title": "邀请码管理",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "下线管理",
+			"icon": "&#xe641;",
+			"href": "offlineAdmin.html"
+		},{
+			"title": "邀请码管理",
+			"icon": "&#xe63c;",
+			"href": "codeAdmin.html"
+		}, {
+			"title": "邀请码返利",
+			"icon": "&#xe63c;",
+			"href": "codeRedate.html"
+		}]
+	},{
+		"title": "系统工具",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "短信工具",
+			"icon": "&#xe641;",
+			"href": "messTool.html"
+		}]
+	},{
+		"title": "用户端app管理",
+		"icon": "",
+		"spread": false,
+		"children": [{
+			"title": "首页管理",
+			"icon": "&#xe641;",
+			"href": "homeAdmin.html"
+		}]
+	}];
